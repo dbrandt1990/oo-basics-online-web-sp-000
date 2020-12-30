abbb41e4674a5e8dbde9a2c73ddd12b6a00f19ea
Danny Brandt <dvbrandt90@gmail.com>
2020-12-30 21:27:06 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,12 @@
 class Book
   attr_accessor :author, :page_count, :assert_generates
   attr_reader :title
+  
   def initilize(title)
     @title =  title
   end
+  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
 end
-
